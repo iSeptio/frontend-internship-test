c02a85f5a042a4a541af657162371b7512a8a9ba
Kamil T <kamil.trojnar@gmail.com>
2019-01-22 20:38:09 +0100

    show pop up on click

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
+
+function loginbarSwitch(show) { document.getElementById('popupbox').style.visibility = show ? "visible" : "hidden"; }
+
+document.getElementById("show-popup-form").onclick = () => loginbarSwitch(1)
+
 /* Here goes your JS code */
