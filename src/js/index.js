1c6213a38e7c5eb77399a9f989b065949d2e20c3
Kamil T <kamil.trojnar@gmail.com>
2019-01-24 11:13:27 +0100

    final

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,29 @@ function loginbarSwitch(show) { document.getElementById('popupbox').style.visibi
 
 function onClick(selector, fn) { document.querySelector(selector).onclick = fn; }
 
+function onSubmit(selector, fn) { document.querySelector(selector).onsubmit = fn; } 
+
+
+function validateForm() {
+    var x = document.forms["login"]["email"].value;
+    var y = document.forms["login"]["password"].value;
+    if (x == "") {
+      alert("Email must be filled out");
+      return false;
+    }
+    
+    else if(y.length<6){
+        alert("Password must be at least 6 characters long.")
+        return false;
+    }
+    else{
+        setTimeout(loginbarSwitch(0),3000) 
+        document.querySelector("#show-popup-form").innerHTML = "Thanks you!"
+    }
+  }
+
+onSubmit("#loginform", () => validateForm())
 onClick("#show-popup-form", () => loginbarSwitch(1))
 onClick("#close-button", () => loginbarSwitch(0))
+
+
